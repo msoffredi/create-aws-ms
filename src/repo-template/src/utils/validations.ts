4b9f8d50ab63c94ba01c8435febd7626845477c4
Marcelo Soffredi <msoffredi@gmail.com>
2023-08-15 10:43:35 -0500

    Updated template.yml

--- src/repo-template/src/utils/validations.ts
+++ src/repo-template/src/utils/validations.ts
@@ -6,22 +6,22 @@ export const validateEnvVars = (): void => {
         console.error('No ENV_TYPE env var defined');
         exit(1);
     }
-
     /// s3-start ///
+
     if (!process.env.BUCKET_NAME) {
         console.error('No BUCKET_NAME env var defined');
         exit(1);
     }
     /// s3-end ///
-
     /// ddb-start ///
+
     if (!process.env.DDB_NAME) {
         console.error('No DDB_NAME env var defined');
         exit(1);
     }
     /// ddb-end ///
-
     /// events-start ///
+
     if (!process.env.EVENT_BUS_NAME) {
         console.error('No EVENT_BUS_NAME env var defined');
         exit(1);
